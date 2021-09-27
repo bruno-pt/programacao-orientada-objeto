2e89d8fe26f5c15b80a0a0e0c203027025f9492a
bruno-pt <bruno.peternella28@gmail.com>
2021-09-27 15:58:52 -0300

    Correção nos construtores

---
+++ Aula18/Ex1/Galgo.java
@@ -0,0 +1,19 @@
+package Aula18.Ex1;
+
+public final class Galgo extends Cachorro{
+    private float velocidade;
+    private String printConstructorGalgo;
+
+    public Galgo(String nome, float peso, String habitos, String cor, String som, float velocidade) {
+        super(nome, peso, habitos, cor, som);
+        this.velocidade = velocidade;
+
+        this.printConstructorGalgo = printConstructorCachorro
+                + " | Velocidade: " + velocidade;
+
+        System.out.println("Constructor Galgo:" + printConstructorGalgo);
+    }
+
+    public void correr(){
+    }
+}
